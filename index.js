95736f645a9c5fda4360d5efa4e7fb18f25f527f
Thomas Noel <tnoel@novadynamics.com>
2023-08-17 18:00:58 -0700

    Implement computer move

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+
+/**
+ * Rock Paper Scissors
+ *
+ * @author Thomas Noel
+ * @date 08/17/23
+ */
+
+
+/**
+ * Computes the computer move
+ */
+function getComputerChoice() {
+    let moves = ["Rock", "Paper", "Scissors"];
+    let choice = Math.floor(Math.random() * moves.length);
+    return moves[choice];
+}
+
+console.log(getComputerChoice());
